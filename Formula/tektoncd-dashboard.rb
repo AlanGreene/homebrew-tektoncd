cdc008e8d0cb942f5c9c6fb873ed8deb94d5dc23
Alan Greene <github.com@alangreene.net>
2022-01-25 17:31:06 +0000

    Update to Tekton Dashboard v0.24.0 UI

--- Formula/tektoncd-dashboard.rb
+++ Formula/tektoncd-dashboard.rb
@@ -1,11 +1,10 @@
 class TektoncdDashboard < Formula
   desc "Tekton Dashboard - The web-based UI for interacting with Tekton"
   homepage "https://github.com/tektoncd/dashboard"
-  version "0.6-alpha"
-  bottle :unneeded
+  version "0.24-alpha"
 
-  url "https://github.com/alangreene/homebrew-tektoncd/releases/download/v0.6-alpha/tekton-dashboard-ui_0.6-alpha.tar.gz"
-  sha256 "edf0e8fbed1540344f02bc32604849ac204aabe957cad2508e321b7a253cd9c0"
+  url "https://github.com/alangreene/homebrew-tektoncd/releases/download/v0.24-alpha/tekton-dashboard-ui_0.24-alpha.tar.gz"
+  sha256 "bec18a835f44d02f69f1650700354c69b13edce15dd05072b5328f11180aba4f"
 
   def install
     libexec.install Dir["*"]
