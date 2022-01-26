67e86b25c1f5f2c443a3ebd307c531a4367636a6
Alan Greene <github.com@alangreene.net>
2022-01-26 14:07:34 +0000

    Dashboard v0.24.1 - fix for blank page in Safari

--- Formula/tektoncd-dashboard.rb
+++ Formula/tektoncd-dashboard.rb
@@ -1,10 +1,10 @@
 class TektoncdDashboard < Formula
   desc "Tekton Dashboard - The web-based UI for interacting with Tekton"
   homepage "https://github.com/tektoncd/dashboard"
-  version "0.24-alpha"
+  version "0.24.1-alpha"
 
-  url "https://github.com/alangreene/homebrew-tektoncd/releases/download/v0.24-alpha/tekton-dashboard-ui_0.24-alpha.tar.gz"
-  sha256 "bec18a835f44d02f69f1650700354c69b13edce15dd05072b5328f11180aba4f"
+  url "https://github.com/alangreene/homebrew-tektoncd/releases/download/v0.24.1-alpha/tekton-dashboard-ui_0.24.1-alpha.tar.gz"
+  sha256 "eb531ade184885b82e7a4feb20bfd33a5894fc83b1542b746a887f48398f0e07"
 
   def install
     libexec.install Dir["*"]
