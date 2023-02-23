baa249d7c0b1801e033d8b8f6dd3a29b3cedd701
Alan Greene <github.com@alangreene.net>
2023-02-23 12:51:25 +0000

    Update sha for fixed bundle and add test block

--- Formula/tektoncd-dashboard.rb
+++ Formula/tektoncd-dashboard.rb
@@ -1,13 +1,16 @@
 class TektoncdDashboard < Formula
   desc "Tekton Dashboard - The web-based UI for interacting with Tekton"
   homepage "https://github.com/tektoncd/dashboard"
-  version "0.33.0-alpha"
-
   url "https://github.com/alangreene/homebrew-tektoncd/releases/download/v0.33.0-alpha/tekton-dashboard-ui_0.33.0-alpha.tar.gz"
-  sha256 "ca566ec4d9581a4bfb02b7312a9aa8aa9348c2196d97f1cb1d52e7322dc2cf32"
+  sha256 "a59bbdd83a6f41aa725d43302b2b7fbf43f5271528b4d56cb5e8f678b61bdbf8"
 
   def install
     libexec.install Dir["*"]
-    bin.write_exec_script (libexec/"tkn-dashboard")
+    bin.write_exec_script libexec/"tkn-dashboard"
+  end
+
+  test do
+    assert_predicate libexec/"tkn-dashboard", :exist?, "tkn-dashboard must exist"
+    assert_predicate libexec/"tkn-dashboard", :executable?, "tkn-dashboard must be executable"
   end
 end
